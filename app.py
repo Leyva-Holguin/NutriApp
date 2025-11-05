3e43609242092a4fe7572e68c3a4b3f592216bd4
Daniel Ivan <24308060610008@cetis61.edu.mx>
2025-11-04 19:36:57 -0700

    1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'la_primera_es_la_primera'
 
 @app.route('/')
 def index():
-    return 'index.html'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
