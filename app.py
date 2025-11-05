7cc1ed9208981e1606904bdbb27fcde4354d5a5e
MelannieAbrilPascualTorres <24308060610062@cetis61.edu.mx>
2025-11-04 19:35:08 -0700

    666

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
 
-app = Flask(__name__)
+app = Flask(__name__) 
+
+app.config['SECRET_KEY'] = 'la_primera_es_la_primera'
 
 @app.route('/')
-def inicio():
+def index():
     return 'index.html'
 
 if __name__ == '__main__':
